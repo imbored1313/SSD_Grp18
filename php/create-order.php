a92ce0a00e073bd07230d284d31df6d316c0e80d
Yin Er <146720279+Eerraaaa@users.noreply.github.com>
2025-07-09 23:02:19 +0800

    fix currency mismatch to SGD

--- php/create-order.php
+++ php/create-order.php
@@ -4,6 +4,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 $amount = $data['amount'];
 $clientId = 'AVyilJ6eWAp302gUQQDI6HVO19xtLy7OGAp7ZUDRwmKY__jhoV6M-Xvdb3-raWXW2uX7wLtLtEbj-nh4';
 $clientSecret = 'EA6aWvSedTU2peoR7zMfdGY23CPEakYbDO4-DTNXSqGXBo_M1PIWLJd0rshmFrXKStV2avtfdmPSS2ih';
+
 // 1. Get access token
 $ch = curl_init('https://api-m.sandbox.paypal.com/v1/oauth2/token');
 curl_setopt_array($ch, [
@@ -16,13 +17,15 @@ curl_setopt_array($ch, [
 $res = curl_exec($ch);
 curl_close($ch);
 $token = json_decode($res, true)['access_token'];
-// 2. Create order
+
+// 2. Create order with SGD currency
 $orderData = [
   'intent' => 'CAPTURE',
   'purchase_units' => [[
-    'amount' => ['currency_code' => 'USD', 'value' => $amount]
+    'amount' => ['currency_code' => 'SGD', 'value' => $amount]  // Changed to SGD
   ]]
 ];
+
 $ch = curl_init('https://api-m.sandbox.paypal.com/v2/checkout/orders');
 curl_setopt_array($ch, [
   CURLOPT_RETURNTRANSFER => true,
