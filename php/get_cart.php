8f6720defc90a7e576728f3e0c38f6d8b65c3c68
Yin Er <146720279+Eerraaaa@users.noreply.github.com>
2025-07-10 16:57:59 +0800

    debugging add to cart issue

--- php/get_cart.php
+++ php/get_cart.php
@@ -1,7 +1,7 @@
 <?php
 // php/get_cart.php - CORRECTED for your exact database schema
 error_reporting(E_ALL);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 
 require_once(__DIR__ . '/config.php');
